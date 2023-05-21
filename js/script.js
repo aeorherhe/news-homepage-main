55ef27a40e64c70db7e868b61228e5ed356f1212
aeorherhe <aeorherhe@gmail.com>
2023-05-21 23:19:53 +0100

    Close Navbr on click of overlay

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 // select html elements
 const openBtn = document.querySelector(".nav-open");
 const closeBtn = document.querySelector(".nav-close img");
+const overlay = document.querySelector(".overlay");
 const main = document.querySelector(".main");
 
 openBtn.addEventListener("click", function () {
   main.classList.add("show-navbar");
 });
 
-closeBtn.addEventListener("click", function () {
-  main.classList.remove("show-navbar");
-});
+function closeNavbar(closeBtns) {
+  closeBtns.forEach(function (close) {
+    close.addEventListener("click", function () {
+      main.classList.remove("show-navbar");
+    });
+  });
+}
+closeNavbar([closeBtn, overlay]);
